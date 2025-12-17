57e6ee28bd3f02b23bff1b6f9a3765433d27888e
excel <excelynomagentaa@gmail.com>
2025-12-17 22:27:49 +0700

    menambahkan tipe data Null

---
+++ dataNULL.php
@@ -0,0 +1,17 @@
+<?php
+//Nilai NULL merepresentasikan sebuah variable tanpa nilai
+//saat kita membuat variable, lalu ingin menghapus data yang terdapat di variable tersebut, kita bisa menggunakan NULL untuk mengosongkan variable tersebut
+// untuk membuat data NULL kita bisa menggunakan kata kunci NULL (Case insensitive)
+
+$name = "excel";
+$name = NULL;
+
+$age = null;
+
+echo "Name : ";
+echo $name;
+echo "\n";
+
+echo "age : ";
+echo $age;
+echo "\n";
