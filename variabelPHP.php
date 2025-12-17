9fa07c507a3e5971cd7b27ff2e012dd694e45ba4
excel <excelynomagentaa@gmail.com>
2025-12-17 21:36:03 +0700

    membuat variabel di php

---
+++ variabelPHP.php
@@ -0,0 +1,13 @@
+<?php
+//variable adalah tempat untuk mentimpan data sehingga bisa kita gunakan lagi di kode program selanjutnya
+//di PHP variable bisa menampung berbagai jenis tipe data dan bisa berubah ubah tipe data 
+// untuk membuat variable kita bisa menggunakan tanda $dolar di ikuti dengan nama variabelnya
+// penamaan variable tidak boleh mengandung spasi
+
+$nama = "Excel";
+$age = 30;
+
+echo "Name : ";
+echo $nama;
+echo "\numur : ";
+echo $age;
