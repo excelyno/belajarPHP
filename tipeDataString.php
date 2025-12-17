714df763ca4cb95edc700e642d49795e30ea081c
excel <excelynomagentaa@gmail.com>
2025-12-17 21:12:01 +0700

    menambahkan tipe data strings

---
+++ tipeDataString.php
@@ -0,0 +1,19 @@
+<?php
+//tipe data string adalah tipe data representasi dari teks
+// string bisa mengandung kosong atau banyak karakter
+
+// single Quote
+//untuk membuat string di PHP, kita bisa menggunakan single quote
+
+echo 'Nama: ';
+echo 'Excel suka makan ikan dengan segalanya ';
+
+//double quote
+//selain single quote kita juga bisa menggunakan double quote. salah satu kelebihan menggunakan double quote adalah, kita menggunakan escape sequwnce untuk beberapa hal seperti \n untuk enter dan \t untuk tab\" untuk double quote dan lain lain
+
+echo 'Nama : ';
+echo 'excel suka ikanhiu';
+echo "\n";
+
+echo "Nama : ";
+echo "excel \t suka\t ikanhiu\n";
