0dfce0055972f9181aebad7047db95f54ae8e250
excel <excelynomagentaa@gmail.com>
2025-12-17 21:05:29 +0700

    menambahkan tipe data boolean

---
+++ tipeDataBoolean.php
@@ -0,0 +1,13 @@
+<?php
+
+// tipe data bolean adalah tipe data paling sederhana di php
+//tipe data boolean adalah tipe data dengan nilai kebenaran (benar atau salah)
+//nilai dipresentasikan dengan true (case insensitive)
+//nilai salah di representasikan dengan false (case insensitive)
+
+// contoh tipe data boolean
+echo "benar : ";
+var_dump(true);
+
+echo "salah : ";
+var_dump(false);
