5ed5e0cbdd8eba652af81400bbb2524203aeeb82
excel <excelynomagentaa@gmail.com>
2025-12-17 21:48:31 +0700

    bingung terhadap variable variables

--- variabelPHP.php
+++ variabelPHP.php
@@ -11,3 +11,14 @@ echo "Name : ";
 echo $nama;
 echo "\numur : ";
 echo $age;
+
+//Variable Variables
+// php mmemiliki kemampuan variable variable yaitu membuat variable dari string value variable
+//walaupun fitur ini ada, tapi fitur ini sangat membingungkan jika digunakan secara luas, jadi disarankan ya jangan dipakai kecuali memang perlu
+//untuk membuat variable dari value variable kita bisa menggunakan $$ di ikuti dengan nama variable nya
+
+$siapa = "aku";
+$$siapa = "keren";
+
+echo "\$siapa = ";
+echo $siapa;
