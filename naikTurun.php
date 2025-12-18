b55a20245c834e73833db835aa5f0fcf12444534
excel <excelynomagentaa@gmail.com>
2025-12-18 14:09:53 +0700

    increment dan decrement

--- naikTurun.php
+++ naikTurun.php
@@ -4,3 +4,14 @@
 //ini bisa mempersingkat kita ketika ingin menaikkan data
 
 //operator increment dan decrement
+// $a++ => post increment => kembalikan nilai $a lalu naikkan 1 angka
+//++$a => pre increment => naikkan nilai $a satu angka lalu kembalikan $a
+//$a-- => post decrement => kembalikan nilai $a lalu turunkan 1 angka
+// --$a => pre decrement => turunkan nilai $a satu angka lalu kembalikan nilai $a
+
+$a = 10;
+$b = $a++;
+
+var_dump($a);
+var_dump($b);
+
