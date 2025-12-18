8e6c9801c105cf5f8c54723b49a5bf1ee6bf3f5c
excel <excelynomagentaa@gmail.com>
2025-12-18 13:58:13 +0700

    menambahkan increment decrement

---
+++ naikTurun.php
@@ -0,0 +1,6 @@
+<?php
+//Increment dan Decrement 
+//PHP mendukkung gaya bahasa pemrograman C untuk menaikkan dan menurunkan data Number sejumlah 1 angka
+//ini bisa mempersingkat kita ketika ingin menaikkan data
+
+//operator increment dan decrement
